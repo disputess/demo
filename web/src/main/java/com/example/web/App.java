9476983d922e4027910a55935d671970712e9daa
宋超杰 <mmuu@mmuu.com>
2020-03-17 17:22:36 +0800

    修改依赖关系

--- web/src/main/java/com/example/web/App.java
+++ web/src/main/java/com/example/web/App.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class App {
 
 	public static void main(String[] args) {
+		System.setProperty("druid.logType","slf4j");
 		SpringApplication.run(App.class, args);
 		log.info("============================App启动完成============================");
 	}
